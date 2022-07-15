b430f0b483d422a9bc081bc27a4bb92151170752
usr38259 <88048930+usr38259@users.noreply.github.com>
2022-07-15 19:06:29 +0300

    Add files via upload

--- ghreq.java
+++ ghreq.java
@@ -5,14 +5,13 @@ import java.nio.charset.Charset;
 
 class ghreq {
 	public static void main (String[] args) {
-		System.out.println ("Program");
+		System.out.println ("GitHub stat request example");
 		try {
-			String token = Files.readAllLines (Paths.get ("ghtoken.txt"), Charset.forName ("US-ASCII")).get (0);
+			String token = new String (Files.readAllBytes (Paths.get ("ghtoken.txt")), Charset.forName ("US-ASCII"));
 			HttpURLConnection conn = (HttpURLConnection)
 				new URL("https://api.github.com/repos/usr38259/gh-stat/traffic/clones").openConnection();
 			conn.setRequestProperty ("Accept", "application/vnd.github+json");
-			conn.setRequestProperty ("Authorization", "token " +
-				token);
+			conn.setRequestProperty ("Authorization", "token " + token);
 			System.out.println (conn.getResponseCode());
 			System.out.println (conn.getResponseMessage());
 		} catch (Exception e) {
