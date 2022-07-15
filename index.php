1877b39f1df46437b1abb5bbaff269f1c824f413
usr38259 <88048930+usr38259@users.noreply.github.com>
2022-07-15 18:58:15 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,272 @@
+<?php
+
+$resp = array(
+<<<'EOD'
+{
+  "count": 173,
+  "uniques": 128,
+  "clones": [
+    {
+      "timestamp": "2016-10-10T00:00:00Z",
+      "count": 2,
+      "uniques": 1
+    },
+    {
+      "timestamp": "2016-10-11T00:00:00Z",
+      "count": 17,
+      "uniques": 16
+    },
+    {
+      "timestamp": "2016-10-12T00:00:00Z",
+      "count": 21,
+      "uniques": 15
+    },
+    {
+      "timestamp": "2016-10-13T00:00:00Z",
+      "count": 8,
+      "uniques": 7
+    },
+    {
+      "timestamp": "2016-10-14T00:00:00Z",
+      "count": 5,
+      "uniques": 5
+    },
+    {
+      "timestamp": "2016-10-15T00:00:00Z",
+      "count": 2,
+      "uniques": 2
+    },
+    {
+      "timestamp": "2016-10-16T00:00:00Z",
+      "count": 8,
+      "uniques": 7
+    },
+    {
+      "timestamp": "2016-10-17T00:00:00Z",
+      "count": 26,
+      "uniques": 15
+    },
+    {
+      "timestamp": "2016-10-18T00:00:00Z",
+      "count": 19,
+      "uniques": 17
+    },
+    {
+      "timestamp": "2016-10-19T00:00:00Z",
+      "count": 19,
+      "uniques": 14
+    },
+    {
+      "timestamp": "2016-10-20T00:00:00Z",
+      "count": 19,
+      "uniques": 15
+    },
+    {
+      "timestamp": "2016-10-21T00:00:00Z",
+      "count": 9,
+      "uniques": 7
+    },
+    {
+      "timestamp": "2016-10-22T00:00:00Z",
+      "count": 5,
+      "uniques": 5
+    },
+    {
+      "timestamp": "2016-10-23T00:00:00Z",
+      "count": 6,
+      "uniques": 5
+    },
+    {
+      "timestamp": "2016-10-24T00:00:00Z",
+      "count": 7,
+      "uniques": 5
+    }
+  ]
+}
+EOD
+,<<<'EOD'
+[
+  {
+    "path": "/github/hubot",
+    "title": "github/hubot: A customizable life embetterment robot.",
+    "count": 3542,
+    "uniques": 2225
+  },
+  {
+    "path": "/github/hubot/blob/master/docs/scripting.md",
+    "title": "hubot/scripting.md at master _ github/hubot _ GitHub",
+    "count": 1707,
+    "uniques": 804
+  },
+  {
+    "path": "/github/hubot/tree/master/docs",
+    "title": "hubot/docs at master _ github/hubot _ GitHub",
+    "count": 685,
+    "uniques": 435
+  },
+  {
+    "path": "/github/hubot/tree/master/src",
+    "title": "hubot/src at master _ github/hubot _ GitHub",
+    "count": 577,
+    "uniques": 347
+  },
+  {
+    "path": "/github/hubot/blob/master/docs/index.md",
+    "title": "hubot/index.md at master _ github/hubot _ GitHub",
+    "count": 379,
+    "uniques": 259
+  },
+  {
+    "path": "/github/hubot/blob/master/docs/adapters.md",
+    "title": "hubot/adapters.md at master _ github/hubot _ GitHub",
+    "count": 354,
+    "uniques": 201
+  },
+  {
+    "path": "/github/hubot/tree/master/examples",
+    "title": "hubot/examples at master _ github/hubot _ GitHub",
+    "count": 340,
+    "uniques": 260
+  },
+  {
+    "path": "/github/hubot/blob/master/docs/deploying/heroku.md",
+    "title": "hubot/heroku.md at master _ github/hubot _ GitHub",
+    "count": 324,
+    "uniques": 217
+  },
+  {
+    "path": "/github/hubot/blob/master/src/robot.coffee",
+    "title": "hubot/robot.coffee at master _ github/hubot _ GitHub",
+    "count": 293,
+    "uniques": 191
+  },
+  {
+    "path": "/github/hubot/blob/master/LICENSE.md",
+    "title": "hubot/LICENSE.md at master _ github/hubot _ GitHub",
+    "count": 281,
+    "uniques": 222
+  }
+]
+EOD
+,<<<'EOD'
+[
+  {
+    "referrer": "Google",
+    "count": 4,
+    "uniques": 3
+  },
+  {
+    "referrer": "stackoverflow.com",
+    "count": 2,
+    "uniques": 2
+  },
+  {
+    "referrer": "eggsonbread.com",
+    "count": 1,
+    "uniques": 1
+  },
+  {
+    "referrer": "yandex.ru",
+    "count": 1,
+    "uniques": 1
+  }
+]
+EOD
+,<<<'EOD'
+{
+  "count": 14850,
+  "uniques": 3782,
+  "views": [
+    {
+      "timestamp": "2016-10-10T00:00:00Z",
+      "count": 440,
+      "uniques": 143
+    },
+    {
+      "timestamp": "2016-10-11T00:00:00Z",
+      "count": 1308,
+      "uniques": 414
+    },
+    {
+      "timestamp": "2016-10-12T00:00:00Z",
+      "count": 1486,
+      "uniques": 452
+    },
+    {
+      "timestamp": "2016-10-13T00:00:00Z",
+      "count": 1170,
+      "uniques": 401
+    },
+    {
+      "timestamp": "2016-10-14T00:00:00Z",
+      "count": 868,
+      "uniques": 266
+    },
+    {
+      "timestamp": "2016-10-15T00:00:00Z",
+      "count": 495,
+      "uniques": 157
+    },
+    {
+      "timestamp": "2016-10-16T00:00:00Z",
+      "count": 524,
+      "uniques": 175
+    },
+    {
+      "timestamp": "2016-10-17T00:00:00Z",
+      "count": 1263,
+      "uniques": 412
+    },
+    {
+      "timestamp": "2016-10-18T00:00:00Z",
+      "count": 1402,
+      "uniques": 417
+    },
+    {
+      "timestamp": "2016-10-19T00:00:00Z",
+      "count": 1394,
+      "uniques": 424
+    },
+    {
+      "timestamp": "2016-10-20T00:00:00Z",
+      "count": 1492,
+      "uniques": 448
+    },
+    {
+      "timestamp": "2016-10-21T00:00:00Z",
+      "count": 1153,
+      "uniques": 332
+    },
+    {
+      "timestamp": "2016-10-22T00:00:00Z",
+      "count": 566,
+      "uniques": 168
+    },
+    {
+      "timestamp": "2016-10-23T00:00:00Z",
+      "count": 675,
+      "uniques": 184
+    },
+    {
+      "timestamp": "2016-10-24T00:00:00Z",
+      "count": 614,
+      "uniques": 237
+    }
+  ]
+}
+EOD
+);
+// router.php
+if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"]))
+    return false;    // сервер возвращает файлы напрямую.
+else {
+$ferr = fopen ('php://stderr', 'wb');
+fprintf ($ferr, "Request\n");
+	$req = $_SERVER["REQUEST_URI"];
+	if (strpos ($req, 'clones')) echo $resp [0];
+	else if (strpos ($req, 'paths')) echo $resp [1];
+	else if (strpos ($req, 'referrers')) echo $resp [2];
+	else if (strpos ($req, 'views')) echo $resp [3];
+	else echo '{ "answer": "Welcome to PHP." }';
+}
+?>
