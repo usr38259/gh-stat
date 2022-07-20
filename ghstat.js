99dba485951889e93da6160103f22c9b4f240e51
usr38259 <88048930+usr38259@users.noreply.github.com>
2022-07-20 20:40:48 +0300

    Add files via upload

--- ghstat.js
+++ ghstat.js
@@ -1,35 +1,35 @@
 
 load ("ghlib.js")
 
-if (arguments.length < 2) {
-	print_usage ()
+if (arguments.length < 1) {
+	printUsage ()
 	quit ()
 }
 
-var f_s = f_c = f_v = f_p = f_r = f_usage = fname = false
+var fS = fC = fV = fP = fR = fUsage = fname = false
 var repo, token, arg, s, sl, p
 for (n = o = 0; n < arguments.length; n ++) {
 	arg = arguments [n]; s = new java.lang.String (s)
 	if (arg [0] == '-') {
-		if (s.compareToIgnoreCase ("--help") == 0) {
-			f_usage = true; break
-		}
+		if (s.compareToIgnoreCase ("--help") == 0 ||
+			s.compareToIgnoreCase ("-help") == 0)
+		{	fUsage = true; break	}
 		sl = arg.length
 		for (p = 1; p < sl; p++)
 		switch (arg [p]) {
-		case "s": f_s = true; break
-		case "f": if (arg [++p] == ":") p++
+		case 's': fS = true; break
+		case 'f': if (arg [++p] == ':') p++
 			if (p < sl) fname = arg.substr (p); else
 				if (++n < arguments.length) fname = arguments [n]; else
-					f_usage = true
+					fUsage = true
 			p = sl; break
-		case "c": f_c = true; break
-		case "v": f_v = true; break
-		case "p": f_p = true; break
-		case "r": f_r = true; break
-		case "h": case "?": f_usage = true; p = al; break
-		default: print ("Warning: unknown option: '" + arg + "'")
-			f_usage = true
+		case 'c': fC = true; break
+		case 'v': fV = true; break
+		case 'p': fP = true; break
+		case 'r': fR = true; break
+		case 'h': case 'H': case '?': fUsage = true; p = sl; break
+		default: print ("Warning: unknown option: '" + arg [p] + "'")
+			fUsage = true
 		}
 		continue
 	}
@@ -37,24 +37,37 @@ for (n = o = 0; n < arguments.length; n ++) {
 	case 0: repo = arg; break
 	case 1: token = arg; break
 	default: print ("Warning: Redundant argument: '" + arg + "'")
-		f_usage = true
+		fUsage = true
 	}
 }
-if (!f_usage && o != 2) {
+if (!fUsage && o != 2) {
 	print ("Error: Required arguments are not set")
-	f_usage = true
+	fUsage = true
 }
-if (f_usage) {
-	print_usage (); quit ()
+if (fUsage) {
+	printUsage (); quit ()
 }
 
-function print_usage ()
+function printUsage ()
 {
 	print ("Get GitHub traffic statistic for last 14 days (using REST API)")
-	print ("  ghstat.php <user>/<repo> <token> [-cvpr] [-s] [-f <stat-file>]")
+	print ("  ghstat.js <user>/<repo> <token> [-cvpr] [-s] [-f <stat-file>]")
 	print ("\t-cvpr\t- select requested stat info:")
 	print ("\t\tc: clones; v: views; p: paths; r: referrers")
 	print ("\t-s\t- use stat file")
 	print ("\t-f <stat-file> - set stat file name")
 }
-print (f_s, f_c, f_v, f_p, f_r, f_usage, fname, repo, token)
+if (!(fC || fV || fP || fR))
+	fC = fV = true;
+if (fname !== false) fS = true
+
+r = checkRepoName (repo)
+if (r !== true) {
+	print ("Error: " + r)
+	java.lang.System.exit (1)
+}
+if (token.length > maxTokenLength) {
+	print ("Error: Token length is too big")
+	java.lang.System.exit (1)
+}
+print (fS, fC, fV, fP, fR, fname, repo, token)
