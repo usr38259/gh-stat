2f9cf2e306e45de69ca99e3a3a738548af909dec
usr38259 <88048930+usr38259@users.noreply.github.com>
2022-07-21 23:46:03 +0300

    Add files via upload

--- ghstat.js
+++ ghstat.js
@@ -1,15 +1,91 @@
 
-load ("ghlib.js")
+var maxRepoNameLen, maxTokenLen, GHApiURL
 
-if (arguments.length < 1) {
+if (typeof maxRepoNameLen == "undefined") maxRepoNameLength = 256
+if (typeof maxTokenLen == "undefined") maxTokenLength = 256
+if (typeof GHApiURL == "undefined") GHApiURL = "https://api.github.com/repos/"
+if (typeof args == "undefined") args = arguments
+
+function checkRepoName (name)
+{
+	var l = name.length, i, c
+	if (l > maxRepoNameLength)
+		return "Repo name is too long"
+	for (i = 0; i < l; i++) {
+		c = name [i]
+		if (!(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9'
+			|| c == '-' || c == '_' || c == '/')) break
+	}
+	if (i < l) return "Repo name contains illegal characters (A-Z 0-9 -_ are valid)"
+	var p = name.indexOf ('/')
+	if (p == -1 || name.lastIndexOf ('/') != p)
+		return "Repo name must be in the form 'user/repo' (separated by a slash)"
+	return true
+}
+
+function GHRequest (req, arr)
+{
+	var lst = new java.util.Vector ()
+	lst.add ("curl"); lst.add ("-s"); lst.add ("-D"); lst.add ("-")
+	lst.add (GHApiURL + repo + req)
+	var pbld = new java.lang.ProcessBuilder (lst)
+	var benv = pbld.environment ()
+	if (!benv.containsKey ("LD_LIBRARY_PATH"))
+		benv.put ("LD_LIBRARY_PATH", "/data/data/com.termux/files/usr/lib")
+	if (!benv.containsKey ("LD_PRELOAD"))
+		benv.put ("LD_PRELOAD", "/data/data/com.termux/files/usr/lib/libtermux-exec.so")
+	var p = pbld.start ()
+	p.waitFor ()
+	if (p.exitValue () > 0) {
+		print ("\nError: Bad curl exit value")
+		java.lang.System.exit (1)
+	}
+	var brd = new java.io.BufferedReader (new java.io.InputStreamReader (p.getInputStream ()))
+	var l = brd.readLine ()
+	if (l == null) {
+		print ("\nError: Bad HTTPS response")
+		java.lang.System.exit (1)
+	}
+	var i = l.indexOf (' ')
+	if (i == -1) {
+		print ("\nError: Bad HTTPS response")
+		java.lang.System.exit (1)
+	}
+	while (i < l.length () && l.codePointAt (i) == 0x20) i++
+	var j = i
+	while (j < l.length () && l.codePointAt (j) >= 0x30 && l.codePointAt (j) <= 0x39) j++
+	var rc = parseInt (l.substring (i, j))
+	while (j < l.length () && l.codePointAt (j) == 0x20) j++
+	if (rc != 200) {
+		print ("\nError: Bad HTTPS response code:", rc, l.substring (j))
+		if (rc == 403) print ("Probably token is incorrect")
+		java.lang.System.exit (1)
+	}
+	while ((l = brd.readLine ()) != null)
+		if (l.length () == 0) break
+	var json = new java.lang.String ()
+	while ((l = brd.readLine ()) != null) json += l + "\n"
+	if (arr) return new org.json.JSONArray (json)
+	return new org.json.JSONObject (json)
+}
+
+function printUsage ()
+{
+	print ("Get GitHub traffic statistic for last 14 days (using REST API)")
+	print ("  ghstat.js <user>/<repo> <token> [-cvpr]")
+	print ("\t-cvpr\t- select requested stat info:")
+	print ("\t\tc: clones; v: views; p: paths; r: referrers")
+}
+
+if (args.length < 1) {
 	printUsage ()
 	quit ()
 }
 
 var fS = fC = fV = fP = fR = fUsage = fname = false
 var repo, token, arg, s, sl, p
-for (n = o = 0; n < arguments.length; n ++) {
-	arg = arguments [n]; s = new java.lang.String (s)
+for (n = o = 0; n < args.length; n ++) {
+	arg = args [n]; s = new java.lang.String (s)
 	if (arg [0] == '-') {
 		if (s.compareToIgnoreCase ("--help") == 0 ||
 			s.compareToIgnoreCase ("-help") == 0)
@@ -20,7 +96,7 @@ for (n = o = 0; n < arguments.length; n ++) {
 		case 's': fS = true; break
 		case 'f': if (arg [++p] == ':') p++
 			if (p < sl) fname = arg.substr (p); else
-				if (++n < arguments.length) fname = arguments [n]; else
+				if (++n < args.length) fname = args [n]; else
 					fUsage = true
 			p = sl; break
 		case 'c': fC = true; break
@@ -48,15 +124,6 @@ if (fUsage) {
 	printUsage (); quit ()
 }
 
-function printUsage ()
-{
-	print ("Get GitHub traffic statistic for last 14 days (using REST API)")
-	print ("  ghstat.js <user>/<repo> <token> [-cvpr] [-s] [-f <stat-file>]")
-	print ("\t-cvpr\t- select requested stat info:")
-	print ("\t\tc: clones; v: views; p: paths; r: referrers")
-	print ("\t-s\t- use stat file")
-	print ("\t-f <stat-file> - set stat file name")
-}
 if (!(fC || fV || fP || fR))
 	fC = fV = true;
 if (fname !== false) fS = true
@@ -70,4 +137,56 @@ if (token.length > maxTokenLength) {
 	print ("Error: Token length is too big")
 	java.lang.System.exit (1)
 }
-print (fS, fC, fV, fP, fR, fname, repo, token)
+var f = true, jo, ja
+function prnok () {
+if (f) { print ("Ok"); f = false }
+}
+function prnList (type) {
+java.lang.System.out.printf ("%3d %s, %d uniques\n",
+	java.lang.Integer.valueOf (jo.optInt ('count')), type,
+	java.lang.Integer.valueOf (jo.optInt ('uniques')))
+ja = jo.optJSONArray (type)
+if (ja != null) {
+	for (var i = 0; (jo = ja.optJSONObject (i)) != null; i++)
+		java.lang.System.out.printf ("  %s\t%5d\t%5d\n",
+			jo.optString ('timestamp').substring (0, 10),
+			java.lang.Integer.valueOf (jo.optInt ('count')),
+			java.lang.Integer.valueOf (jo.optInt ('uniques')))
+}
+}
+function doRequest () {
+java.lang.System.out.print ("\nRequesting ")
+java.lang.System.out.print (repo)
+java.lang.System.out.print ("... ")
+if (fC) {
+jo = GHRequest ("/traffic/clones", false)
+prnok ()
+prnList ('clones')
+}
+if (fP) {
+ja = GHRequest ("/traffic/popular/paths", true)
+prnok ()
+print ("Popular paths:")
+for (var i = 0; (jo = ja.optJSONObject (i)) != null; i++)
+	java.lang.System.out.printf ('%5d %5d  "%s"\n\t\t%s\n',
+		java.lang.Integer.valueOf (jo.optInt ('count')),
+		java.lang.Integer.valueOf (jo.optInt ('uniques')),
+		jo.optString ('title'), jo.optString ('path'))
+}
+if (fR) {
+ja = GHRequest ("/traffic/popular/referrers", true)
+prnok ()
+print ("Popular referrers:")
+for (var i = 0; (jo = ja.optJSONObject (i)) != null; i++)
+	java.lang.System.out.printf ('%5d %5d  %s\n',
+		java.lang.Integer.valueOf (jo.optInt ('count')),
+		java.lang.Integer.valueOf (jo.optInt ('uniques')),
+		jo.optString ('referrer'))
+}
+if (fV) {
+jo = GHRequest ("/traffic/views", false)
+prnok ()
+prnList ('views')
+} }
+
+doRequest (); quit ()
